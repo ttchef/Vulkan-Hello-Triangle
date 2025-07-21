69a38b5369e92afacedb62d4a9e4724a65618118
ttchef <ruro0.edit@gmail.com>
2025-07-21 14:53:58 +0200

    main file better structuring

--- src/main.c
+++ src/main.c
@@ -11,6 +11,79 @@
 
 #include "../include/vulkan_base.h"
 
+VulkanContext* context;
+VkSurfaceKHR surface;
+VulkanSwapchain swapchain;
+VkRenderPass renderPass;
+VkFramebuffer* framebuffers;
+
+void initApplication(GLFWwindow* window) {
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    const char* enabledDeviceExtensions[] = {
+        "VK_KHR_swapchain"
+    };
+
+    context = initVulkan(glfwExtensionCount, glfwExtensions, ARRAY_COUNT(enabledDeviceExtensions), enabledDeviceExtensions);
+    if (!context) {
+        fprintf(stderr, "Failed to create vulkan context!\n");
+        return;
+    }
+
+    surface = VK_NULL_HANDLE;
+    if (glfwCreateWindowSurface(context->instance, window, NULL, &surface) != VK_SUCCESS) {
+        fprintf(stderr, "Failed to create window surface!\n");
+        return;
+    }
+
+    swapchain = createSwapchain(context, surface, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
+
+    renderPass = createRenderPass(context, swapchain.format);
+    framebuffers = malloc(sizeof(VkFramebuffer) * swapchain.imagesCount);
+    if (!framebuffers) {
+        fprintf(stderr, "Failed to allocate memory for framebuffers!\n");
+        return;
+    }
+
+    for (uint32_t i = 0; i < swapchain.imagesCount; i++) {
+        VkFramebufferCreateInfo createInfo = {0};
+        createInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        createInfo.renderPass = renderPass;
+        createInfo.attachmentCount = 1;
+        createInfo.pAttachments = &swapchain.imageViews[i];
+        createInfo.width = swapchain.width;
+        createInfo.height = swapchain.height;
+        createInfo.layers = 1;
+
+        if (vkCreateFramebuffer(context->device, &createInfo, NULL, &framebuffers[i]) != VK_SUCCESS) {
+            fprintf(stderr, "Failed to create vulkan framebuffers!\n");
+            return;
+        }
+    }   
+}
+
+void renderApplication(VulkanContext) {
+
+}
+
+void shutdownApplication() {
+    vkDeviceWaitIdle(context->device);
+    for (uint32_t i = 0; i < swapchain.imagesCount; i++) {
+        vkDestroyFramebuffer(context->device, framebuffers[i], NULL);
+    }
+
+    destroyRenderPass(context, renderPass);
+    destroySwapchain(context, &swapchain);
+    vkDestroySurfaceKHR(context->instance, surface, NULL);
+    exitVulkan(context);
+    free(framebuffers);
+    free(context);
+
+}
+
 int main() {
 
     if (!glfwInit()) {
@@ -27,67 +100,13 @@ int main() {
         return -1;
     }
 
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions;
-
-    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-    const char* enabledDeviceExtensions[] = {
-        "VK_KHR_swapchain"
-    };
-
-    VulkanContext* context = initVulkan(glfwExtensionCount, glfwExtensions, ARRAY_COUNT(enabledDeviceExtensions), enabledDeviceExtensions);
-    if (!context) {
-        fprintf(stderr, "Failed to create vulkan context!\n");
-        return -1;
-    }
-
-    VkSurfaceKHR surface = VK_NULL_HANDLE;
-    if (glfwCreateWindowSurface(context->instance, window, NULL, &surface) != VK_SUCCESS) {
-        fprintf(stderr, "Failed to create window surface!\n");
-        return -1;
-    }
-
-    VulkanSwapchain swapchain = createSwapchain(context, surface, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
-
-    VkRenderPass renderPass = createRenderPass(context, swapchain.format);
-    VkFramebuffer* framebuffers = malloc(sizeof(VkFramebuffer) * swapchain.imagesCount);
-    if (!framebuffers) {
-        fprintf(stderr, "Failed to allocate memory for framebuffers!\n");
-        return -1;
-    }
-
-    for (uint32_t i = 0; i < swapchain.imagesCount; i++) {
-        VkFramebufferCreateInfo createInfo = {0};
-        createInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-        createInfo.renderPass = renderPass;
-        createInfo.attachmentCount = 1;
-        createInfo.pAttachments = &swapchain.imageViews[i];
-        createInfo.width = swapchain.width;
-        createInfo.height = swapchain.height;
-        createInfo.layers = 1;
-
-        if (vkCreateFramebuffer(context->device, &createInfo, NULL, &framebuffers[i]) != VK_SUCCESS) {
-            fprintf(stderr, "Failed to create vulkan framebuffers!\n");
-            return -1;
-        }
-    }   
+    initApplication(window);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
-    vkDeviceWaitIdle(context->device);
-    for (uint32_t i = 0; i < swapchain.imagesCount; i++) {
-        vkDestroyFramebuffer(context->device, framebuffers[i], NULL);
-    }
-
-    destroyRenderPass(context, renderPass);
-    destroySwapchain(context, &swapchain);
-    vkDestroySurfaceKHR(context->instance, surface, NULL);
-    exitVulkan(context);
-    free(framebuffers);
-    free(context);
+    shutdownApplication();
 
     glfwDestroyWindow(window);
     glfwTerminate();
